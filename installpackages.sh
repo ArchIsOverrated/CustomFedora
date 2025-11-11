31244ada10a209b7359a9e911a6c55ee851965e8
ArchIsOverrated <243266206+ArchIsOverrated@users.noreply.github.com>
2025-11-11 14:37:02 -0700

    fixing some issues

--- installpackages.sh
+++ installpackages.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-alias zypper='zyppper --non-interactive --recommends'
-
-zypper update
-zypper install xorg-x11-server labwc lightdm grim rofi-wayland swaybg waybar
+zypper -n update
+zypper -n install xorg-x11-server labwc lightdm grim rofi-wayland swaybg waybar
 update-alternatives --set default-displaymanager /usr/sbin/lightdm
-zypper install -t pattern kvm_server kvm_tools
+zypper -n install -t pattern kvm_server kvm_tools
 systemctl enable --now libvirtd
 systemctl enable --now virtlogd
 usermod -aG libvirt,kvm $USER
