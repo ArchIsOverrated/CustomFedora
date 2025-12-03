174e3b3e92e44b861dd818d4fad009e67beb049e
ArchIsOverrated <243266206+ArchIsOverrated@users.noreply.github.com>
2025-12-02 20:03:50 -0700

    adding some changes to configure vm, need to create a new directory inside of the example for the begin and end event, probably need to modify the hugepages.sh

--- src/configure_vm.sh
+++ src/configure_vm.sh
@@ -8,8 +8,21 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 configure_hooks() {
+  # Create hooks directory if it doesn't exist
+  mkdir -p "$QEMU_HOOKS_DIR"
+
+  VM_DIR
+  echo "Hooks configured successfully in $QEMU_HOOKS_DIR"
+
+  mkdir -p "$VM_DIR"
   
 }
+
+HOOKS_DIR="../hooks"
+QEMU_HOOKS_DIR="$HOOKS_DIR/qemu.d"
+VM_DIR="$QEMU_HOOKS_DIR/$1"
+
+configure_hooks
 
 trap 'echo "Error on line $LINENO while running: $BASH_COMMAND" >&2' ERR
 
